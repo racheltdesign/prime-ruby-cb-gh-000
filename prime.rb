3d4be607d49f35335e703890aabd0241a6e419eb
Rachel Thompson <thompr@pba.edu>
2017-08-15 23:28:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  case
+  when num < 0 && range = (num..-1)
+    when num == 0 || num == 1
+      return false
+    else
+      range = (2..num - 1).to_a
+     end
+     !range.any? do |item|
+     num % item == 0 && (item != 1 || item != -1)
+  end
+end
